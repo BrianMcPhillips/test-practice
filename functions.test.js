cb3fb6ac13a0dd55bfc02cc735fa2b264b2fa919
Brian McPhillips <B.McPhillips89@gmail.com>
2021-08-05 02:19:52 -0700

    Null test and not test

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,13 @@
 const functions = require('./functions');
 
 test('Adds 2 + 2 to equal 4', () => {
-  expect(functions.add(2, 2)).toEqual(4);
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+test('Adds 2 + 2 to Not equal 5', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+test('Should be Null', () => {
+  expect(functions.isNull()).toBeNull();
 });
